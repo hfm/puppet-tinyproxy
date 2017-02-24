2764b2812111054810adc6778a337a99274b79d2
OKUMURA Takahiro <hfm.garden@gmail.com>
2017-02-24 22:17:02 +0900

    Unit testing for CentOS7, Debian8(jessie), and Ubuntu 16.04(Xenial)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 describe 'tinyproxy' do
-  context 'with default values for all parameters' do
-    it { should contain_class('tinyproxy') }
+  on_supported_os.each do |os, facts|
+    context "with default values for all parameters on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile }
+      it { should contain_class('tinyproxy') }
+    end
   end
 end
