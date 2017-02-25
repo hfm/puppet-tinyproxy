aa6f2fa7088344ceafb8b78861c1b8f6d212cea9
OKUMURA Takahiro <hfm.garden@gmail.com>
2017-02-26 02:44:23 +0900

    Add tinyproxy::config for tinyproxy.conf

---
+++ manifests/config.pp
@@ -0,0 +1,28 @@
+class tinyproxy::config (
+  String $config_ensure,
+){
+
+  $config_path = $facts['os']['family'] ? {
+    'Debian' => '/etc/tinyproxy.conf',
+    'RedHat' => '/etc/tinyproxy/tinyproxy.conf',
+    default  => '/etc/tinyproxy/tinyproxy.conf',
+  }
+
+  $tinyproxy_user = $facts['os']['family'] ? {
+    'Debian' => 'nobody',
+    'RedHat' => 'tinyproxy',
+    default  => 'tinyproxy',
+  }
+
+  $tinyproxy_group = $facts['os']['family'] ? {
+    'Debian' => 'nogroup',
+    'RedHat' => 'tinyproxy',
+    default  => 'tinyproxy',
+  }
+
+  file { $config_path:
+    ensure  => $config_ensure,
+    content => template('tinyproxy/tinyproxy.conf.erb'),
+  }
+
+}
